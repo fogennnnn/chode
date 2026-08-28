054e99f1b72884fc711c9d2d5ba91bc14bd7d888
Robert Fogeborg <fogeboro@gmail.com>
2026-08-29 00:20:33 +0200

    chore: remove temp test file

--- test_reverse.py
+++
@@ -1,8 +0,0 @@
-def reverse_string(s):
-    return s[::-1]
-
-# Test
-assert reverse_string("hello") == "olleh"
-assert reverse_string("") == ""
-assert reverse_string("a") == "a"
-print("Python reverse_string: PASS")
